92cab598f9c599295f53f348f72d5ccbb02308bf
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-18 17:30:32 +0700

    menambahkan crud ke dalam DokterDao.kt

--- app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
@@ -1,4 +1,24 @@
 package com.example.ucp2pam.data.dao
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2pam.data.entity.Dokter
+
 interface DokterDao {
+    @Insert
+    suspend fun insertDokter(dokter: Dokter)
+
+    @Query("SELECT * FROM dokter")
+    suspend fun getAllDokter(): List<Dokter>
+
+    @Update
+    suspend fun updateBarang(dokter: Dokter)
+
+    @Delete
+    suspend fun deleteBarang(dokter: Dokter)
+
+    @Query("SELECT * FROM dokter WHERE id = :id")
+    suspend fun getDetailDokter(id: Int): Dokter
 }
