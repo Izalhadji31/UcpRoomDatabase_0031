3584203102652712a29c0a7220fa6918a20b6979
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-18 17:35:02 +0700

    memasukan insert data pada JadwalDao.kt

--- app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
@@ -1,4 +1,13 @@
 package com.example.ucp2pam.data.dao
 
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2pam.data.entity.Jadwal
+
 interface JadwalDao {
+    @Insert
+    suspend fun insertJadwal(jadwal: Jadwal)
+
+    @Query("SELECT * FROM jadwal")
+    suspend fun getAllJadwalr(): List<Jadwal>
 }
