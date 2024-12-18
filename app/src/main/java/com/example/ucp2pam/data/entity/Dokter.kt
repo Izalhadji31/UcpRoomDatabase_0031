9767ca63d5c35b7dde155b7a58e4c9ba63844455
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-18 17:18:45 +0700

    memasukan atribut pada data class Dokter.kt

--- app/src/main/java/com/example/ucp2pam/data/entity/Dokter.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Dokter.kt
@@ -1,5 +1,12 @@
 package com.example.ucp2pam.data.entity
 
+@Entity(tableName = "dokter")
 data class Dokter(
-
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val spesialis: String,
+    val klinik: String,
+    val NoHp : String,
+    val JamKerja: String
 )
