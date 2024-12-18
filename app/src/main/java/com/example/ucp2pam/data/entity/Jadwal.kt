e66d544948f934e72c306ccf4f1c3fbb5e4625cc
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-18 17:28:38 +0700

    memasukan atribut pada data class jadwal

--- app/src/main/java/com/example/ucp2pam/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Jadwal.kt
@@ -1,3 +1,15 @@
 package com.example.ucp2pam.data.entity
 
-data class Jadwal()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Jadwal")
+data class Jadwal(
+    @PrimaryKey(autoGenerate = true)
+    val idjadwal: Int = 0,
+    val namaDokter: String,
+    val namaPasien: String,
+    val noHp: String,
+    val tanggalKonsultasi: String,
+    val Status: String
+)
