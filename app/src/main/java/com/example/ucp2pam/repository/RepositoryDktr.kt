f660d38f9958541ebaaf25af910f3c2e896fcf2c
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-21 12:59:43 +0700

    memperbaiki kesalahan pada LocalRepositoryDktr.kt

--- app/src/main/java/com/example/ucp2pam/repository/RepositoryDktr.kt
+++ app/src/main/java/com/example/ucp2pam/repository/RepositoryDktr.kt
@@ -4,11 +4,11 @@ import com.example.ucp2pam.data.dao.DokterDao
 import com.example.ucp2pam.data.entity.Dokter
 
 interface RepositoryDktr {
-    class DokterRepository(private val dao: DokterDao) {
-        suspend fun insertDokter(dokter: Dokter) = dao.insertDokter(dokter)
-        suspend fun getAllDokter() = dao.getAllDokter()
-        suspend fun updateDokter(dokter: Dokter) = dao.updateDokter(dokter)
-        suspend fun deleteDokter(dokter: Dokter) = dao.deleteDokter(dokter)
-        suspend fun getDokterById(idDokter: Int) = dao.getDokterById(idDokter)
-    }
+        class DokterRepository(private val dao: DokterDao) {
+                suspend fun insertDokter(dokter: Dokter) = dao.insertDokter(dokter)
+                suspend fun getAllDokter() = dao.getAllDokter()
+                suspend fun updateDokter(dokter: Dokter) = dao.updateDokter(dokter)
+                suspend fun deleteDokter(dokter: Dokter) = dao.deleteDokter(dokter)
+                suspend fun getDetailDokter(idDokter: Int) = dao.getDetailDokter(idDokter)
+        }
 }
