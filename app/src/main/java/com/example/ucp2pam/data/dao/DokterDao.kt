98280332eb21a24db4674cbd245c51c010286dbf
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-22 09:20:10 +0700

    Memperbaharui atribut pada DokterDao.kt

--- app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
@@ -1,24 +1,14 @@
 package com.example.ucp2pam.data.dao
 
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
 import com.example.ucp2pam.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
 
 interface DokterDao {
     @Insert
     suspend fun insertDokter(dokter: Dokter)
 
-    @Query("SELECT * FROM dokter")
-    suspend fun getAllDokter(): List<Dokter>
-
-    @Update
-    suspend fun updateDokter(dokter: Dokter)
-
-    @Delete
-    suspend fun deleteDokter(dokter: Dokter)
-
-    @Query("SELECT * FROM dokter WHERE idDokter = :id")
-    suspend fun getDetailDokter(id: Int): Dokter
+    @Query("SELECT * FROM dokter ORDER BY idDokter ASC ")
+    suspend fun getAllDokter(): Flow<List<Dokter>>
 }
