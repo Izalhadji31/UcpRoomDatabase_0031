3d609b1731a68a4383e6bac09664034893b61ca7
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-22 09:38:25 +0700

    Menambahkan prakdokdatabase pada AppDatabase.kt

--- app/src/main/java/com/example/ucp2pam/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/ucp2pam/data/database/AppDatabase.kt
@@ -25,7 +25,7 @@ abstract class AppDatabase : RoomDatabase() {
                 Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "RsDatabase"
+                    "PrakdokDatabase"
                 )
                     .build()
                     .also { instance = it }
