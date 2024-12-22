8b802a4eba458bd277dce62cd6289d6cbad2d6df
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-22 09:37:56 +0700

    Memperbaharui atribut pada Dokter.kt

--- app/src/main/java/com/example/ucp2pam/data/entity/Dokter.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Dokter.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "dokter")
 data class Dokter(
     @PrimaryKey
-    val idDokter: Dokter,
+    val idDokter:Int = 0,
     val namaDokter: String,
     val spesialis: String,
     val klinik: String,
