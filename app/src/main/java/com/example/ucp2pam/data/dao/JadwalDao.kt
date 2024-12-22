099b15d07d736780be8d130bf4754b91405c3837
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-22 09:37:32 +0700

    Memperbaharui atribut pada JadwalDao.kt

--- app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
@@ -1,13 +1,29 @@
 package com.example.ucp2pam.data.dao
 
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2pam.data.entity.Dokter
 import com.example.ucp2pam.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
 
 interface JadwalDao {
     @Insert
     suspend fun insertJadwal(jadwal: Jadwal)
 
-    @Query("SELECT * FROM jadwal")
-    suspend fun getAllJadwal(): List<Jadwal>
+    @Update
+    suspend fun updateJadwal(jadwal: Jadwal)
+
+    @Delete
+    suspend fun deleteJadwal(jadwal: Jadwal)
+
+    @Query("SELECT * FROM jadwal ORDER BY namaPasien ASC")
+    suspend fun getAllJadwal(): Flow<List<Jadwal>>
+
+    @Query("SELECT * FROM jadwal WHERE idJadwal = :idJadwal")
+    fun getJadwal(idJadwal: Int): Flow<Jadwal>
+
+    @Query("SELECT * FROM dokter ORDER BY namaDokter DESC")
+    fun getAllNamaDokter(): Flow<List<Dokter>>
 }
