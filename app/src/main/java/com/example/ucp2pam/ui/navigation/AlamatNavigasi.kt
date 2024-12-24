645c3680860ff18aaf84491384389dc3284f9e09
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-24 08:20:14 +0700

    menambahkan file dan code AlamatNavigasi.kt

---
+++ app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,37 @@
+package com.example.ucp2pam.ui.navigation
+
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinationHome : AlamatNavigasi{
+    override val route = "home"
+}
+// destinaton Dokter
+object DestinationDokterHome : AlamatNavigasi{
+    override val route = "home_dokter"
+}
+
+object DestinationDokterInsert : AlamatNavigasi{
+    override val route = "insert_dokter"
+}
+
+// destinaton Jadwal
+object DestinationJadwalInsert : AlamatNavigasi{
+    override val route = "insert_jadwal"
+}
+object DestinationJadwalHome : AlamatNavigasi{
+    override val route = "home_jadwal"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail_jadwal"
+    const val idJadwal = "idJadwal"
+    val routeWithArg = "$route/{$idJadwal}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update_jadwal"
+    const val idjadwal = "idJadwal"
+    val routeWithArg = "$route/{$idjadwal}"
+}
