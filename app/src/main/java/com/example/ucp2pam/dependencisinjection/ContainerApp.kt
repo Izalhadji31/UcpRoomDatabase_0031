bfadc37721d69453f770f43bd6229deb2fc02e71
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-24 08:13:49 +0700

    memperbahurui data pada ContainerApp.kt

--- app/src/main/java/com/example/ucp2pam/dependencisinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2pam/dependencisinjection/ContainerApp.kt
@@ -1,2 +1,23 @@
 package com.example.ucp2pam.dependencisinjection
 
+import android.content.Context
+import com.example.ucp2pam.data.database.PrakdokDatabase
+import com.example.ucp2pam.repository.LocalRepositoryDktr
+import com.example.ucp2pam.repository.RepositoryDktr
+
+import com.example.ucp2pam.repository.LocalRepositoryJdwl
+import com.example.ucp2pam.repository.RepositoryJdwl
+
+interface InterfaceContainerApp {
+    val repositoryJdwl: RepositoryJdwl
+    val repositoryDktr: RepositoryDktr
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp{
+    override val repositoryDktr: RepositoryDktr by lazy {
+        LocalRepositoryDktr(PrakdokDatabase.getDatabase(context).DokterDao())
+    }
+    override val repositoryJdwl: RepositoryJdwl by lazy {
+        LocalRepositoryJdwl(PrakdokDatabase.getDatabase(context).JadwalDao())
+    }
+}
