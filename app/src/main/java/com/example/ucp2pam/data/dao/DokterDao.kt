fad99da265b5f359284932c1384550aaa7e72e81
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-24 08:09:36 +0700

    memperbaiki bug pada  atribut yang ada di DokterDao.kt. dan JadwalDao.kt

--- app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/DokterDao.kt
@@ -1,14 +1,22 @@
 package com.example.ucp2pam.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.ucp2pam.data.entity.Dokter
 import kotlinx.coroutines.flow.Flow
 
+
+@Dao
 interface DokterDao {
     @Insert
     suspend fun insertDokter(dokter: Dokter)
 
-    @Query("SELECT * FROM dokter ORDER BY idDokter ASC ")
-    suspend fun getAllDokter(): Flow<List<Dokter>>
+    @Query("SELECT * FROM dokter ORDER BY namaDokter ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
+
+    @Query("SELECT * FROM dokter WHERE idDokter = :idDokter")
+    fun getDokter(idDokter : String): Flow<Dokter>
 }
--- app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
@@ -1,5 +1,6 @@
 package com.example.ucp2pam.data.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
@@ -8,21 +9,22 @@ import com.example.ucp2pam.data.entity.Dokter
 import com.example.ucp2pam.data.entity.Jadwal
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface JadwalDao {
     @Insert
-    suspend fun insertJadwal(jadwal: Jadwal)
-
-    @Update
-    suspend fun updateJadwal(jadwal: Jadwal)
+    suspend fun insertJadwal (jadwal: Jadwal)
 
     @Delete
-    suspend fun deleteJadwal(jadwal: Jadwal)
+    suspend fun deleteJadwal (jadwal: Jadwal)
 
-    @Query("SELECT * FROM jadwal ORDER BY namaPasien ASC")
-    suspend fun getAllJadwal(): Flow<List<Jadwal>>
+    @Update
+    suspend fun updateJadwal (jadwal: Jadwal)
+
+    @Query("SELECT * FROM jadwal ORDER BY idJadwal ASC")
+    fun getAllJadwal(): Flow<List<Jadwal>>
 
     @Query("SELECT * FROM jadwal WHERE idJadwal = :idJadwal")
-    fun getJadwal(idJadwal: Int): Flow<Jadwal>
+    fun getJadwal(idJadwal : String): Flow<Jadwal>
 
     @Query("SELECT * FROM dokter ORDER BY namaDokter DESC")
     fun getAllNamaDokter(): Flow<List<Dokter>>
